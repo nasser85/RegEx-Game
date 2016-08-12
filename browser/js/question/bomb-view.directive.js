a491fd882179d0eff3dc1a32d050c000e1185d47
Seungjae Moon <sjaymoon15@gmail.com>
2016-08-12 18:33:20 -0400

    move bomb-view to play.html and slidein animation css

--- browser/js/question/bomb-view.directive.js
+++ browser/js/question/bomb-view.directive.js
@@ -2,5 +2,9 @@ app.directive('bombView', function(){
 	return {
 		restrict: "E",
 		templateUrl: 'js/question/question.html'
+		// link: function(scope, elem, attrs){
+		// 	elem.removeAttr('class').addClass('magictime twisterInDown');
+		// 	// class="magictime slideRightRetourn"
+		// }
 	}
 })
