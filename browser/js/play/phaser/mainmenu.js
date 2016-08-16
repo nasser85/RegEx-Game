bc70410106bfe84fa951a6bfd58badb7142551a3
ishdaddy7 <seanhan1@gmail.com>
2016-08-15 22:37:35 -0400

    tiny tweak to placeholder game menu content

--- browser/js/play/phaser/mainmenu.js
+++ browser/js/play/phaser/mainmenu.js
@@ -10,16 +10,21 @@ RegexGame.MainMenu.prototype = {
     this.background = this.game.add.sprite(0, 0, 'desert');
 
     //start game text
-    var text = "TAP TO BEGIN";
+    var text = "LEVEL 1 >";
     var style = { font: "30px Arial", fill: "#000", align: "center" };
     var t = this.game.add.text(this.game.width/2, this.game.height/2, text, style);
+    t.anchor.set(0.5);
+
+    var text = "LEVEL 2 >";
+    var style = { font: "30px Arial", fill: "#000", align: "center" };
+    var t = this.game.add.text(this.game.width/2, this.game.height/2+50, text, style);
     t.anchor.set(0.5);
 
     //highest score
     text = "Highest score: "+this.highestScore;
     style = { font: "15px Arial", fill: "#000", align: "center" };
 
-    var h = this.game.add.text(this.game.width/2, this.game.height/2 + 50, text, style);
+    var h = this.game.add.text(this.game.width/2, this.game.height/2 + 100, text, style);
     h.anchor.set(0.5);
   },
   update: function() {
