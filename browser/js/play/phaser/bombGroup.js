e454460013807157bffc9d551b393d8fd19bff43
ishdaddy7 <seanhan1@gmail.com>
2016-08-18 11:12:54 -0400

    extended time to answer questions

--- browser/js/play/phaser/bombGroup.js
+++ browser/js/play/phaser/bombGroup.js
@@ -13,7 +13,7 @@ var BombGroup = function (game, arrQuestions, image) {
     game.physics.enable(sprite, Phaser.Physics.ARCADE);
     sprite.body.gravity.y = 300;
     sprite.question = arrQuestions[i];
-    sprite.expirationTime = Date.now() + 10000 + 1000*(20*i);
+    sprite.expirationTime = Date.now() + 30000 + 1000*(20*i);
     if(sprite.expirationTime > RegexGame.gameConfig.timeLimit) RegexGame.gameConfig.timeLimit = sprite.expirationTime;
     var bombTimer = new Timer(game, sprite);
     sprite.enableBody = true;
