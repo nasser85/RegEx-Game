f8a51da6dc49b51b1b4766ef6ae586be66c98eb1
Seungjae Moon <sjaymoon15@gmail.com>
2016-08-30 11:48:28 -0400

    fix margin css for end-game-sign-up

--- browser/js/play/phaser/phaserState_GameOver.js
+++ browser/js/play/phaser/phaserState_GameOver.js
@@ -20,7 +20,9 @@ RegexGame.GameOver.prototype = {
   init: function(){
       this.groan = this.add.audio('groan');
       this.groan.addMarker('playGroan',0,2);
-      this.game.scope.saveToDatabase(this.game.scope.score, this.game.scope.user.id);
+      if(this.game.scope.user){
+        this.game.scope.saveToDatabase(this.game.scope.score, this.game.scope.user.id);
+      }
     },
   create: function() {
     let background = this.game.add.tilemap('simpleCity_Layer1');
